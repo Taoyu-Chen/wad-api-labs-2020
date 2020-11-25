e67058cd07ae71c15193383694d13c3cafcfe59b
Taoyu-Chen <875693150>
2020-11-25 18:45:10 +0800

    added routing for add,update,delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,15 +1,24 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
+
+
 const app = express();
 
+//configure body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// eslint-disable-next-line no-undef
 const port = process.env.PORT;
 
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
+
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
