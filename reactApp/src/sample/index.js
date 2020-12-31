fd496dce2a29a9a8492039339699ec41bad42469
SevenSound <875693150@qq.com>
2020-12-31 23:01:35 +0800

    Topic 11 - I've completed all my labs

--- reactApp/src/sample/index.js
+++ reactApp/src/sample/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 import { PublicPage, Movies, Profile, HomePage } from "./pages";
+import MovieProvider from "./moviesContext";
 import LoginPage from "./loginPage";
 import SignUpPage from "./signUpPage";
 import PrivateRoute from "./privateRoute";
@@ -11,32 +12,34 @@ import AuthProvider from "./authContext";
 const App = () => {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <AuthHeader />
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/public">Public</Link>
-          </li>
-          <li>
-            <Link to="/movies">Movies</Link>
-          </li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route path="/public" component={PublicPage} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/signup" component={SignUpPage} />,
-          <Route exact path="/" component={HomePage} />
-          <PrivateRoute path="/movies" component={Movies} />
-          <PrivateRoute path="/profile" component={Profile} />
-          <Redirect from="*" to="/" />
-        </Switch>
-      </AuthProvider>
+      <MovieProvider>
+        <AuthProvider>
+          <AuthHeader />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/public">Public</Link>
+            </li>
+            <li>
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/public" component={PublicPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/signup" component={SignUpPage} />,
+            <Route exact path="/" component={HomePage} />
+            <PrivateRoute path="/movies" component={Movies} />
+            <PrivateRoute path="/profile" component={Profile} />
+            <Redirect from="*" to="/" />
+          </Switch>
+        </AuthProvider>
+      </MovieProvider>
     </BrowserRouter>
   );
 };
